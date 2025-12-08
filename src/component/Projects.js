1fe686095b803f1aa340582f35b986aa8f34caa5
arvindsilswal009-svg <arvindsilswal009@gmail.com>
2025-12-08 21:36:59 +0530

    navbar changes

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -7,7 +7,7 @@ import react from "react";
             <div className="carousel owl-carousel">
               <div className="card">
                 <div className="box">
-                  <img src="src\weatherapp.jpg" alt=""/>
+                  <img src="calculator.jpg" alt=""/>
                   <div className="  text">Calculator</div>
                   <p>This is a Calculator app for calculating mathematical calculations .
                     technologies used in developing the calculator are html, css, Bootstrap,JavaScript, react.js .
@@ -18,7 +18,7 @@ import react from "react";
               </div>
                 <div className="card">
                 <div className="box">
-                  <img src="" alt=""/>
+                  <img src="weatherapp" alt=""/>
                   <p>This is a weather app website developed using Html , css , bootstrap ,javascript ,and with library like React .js . we can know teh weather conditions of any city just by searching the city name,it shows current weather condition with the help of weather api</p>
                    <a className="btn" href=" https://arvindsilswal009-svg.github.io/Weatherapp/">Live Link</a>&emsp;&emsp;
                             <a className="btn" href="https://github.com/arvindsilswal009-svg/Weatherapp">Code Link</a>
@@ -27,7 +27,7 @@ import react from "react";
               </div>
               <div className="card">
                 <div className="box">
-                  <img src="/images/portfolio.jpg" alt=""/>
+                  <img src="portfolio.jpg" alt=""/>
                   <div className="text">Portfolio</div>
                   <p>Here is my portfolio.</p>
                    <a className="btn" href=" https://arvindsilswal009-svg.github.io/MYportfolio/">Live Link</a>&emsp;&emsp;
