ada6b55a53c34637f317fd90cbc035363c19315a
arvindsilswal009-svg <arvindsilswal009@gmail.com>
2025-12-09 19:12:01 +0530

    project potos added

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -9,31 +9,44 @@
                 <div className="contactcard">
                  <div className="row">
                         <div className="info">
-                    <div className="icons">   <i className="fa-solid fa-user-large"></i>
-
+                    <div className="icons">   
+                     <ul style={{display:"flex", listStyle:"none" }}><li><i className="fa-solid fa-user-large"></i></li>
+                      <li>
                         <div className="sub-title"><a style={{textdecoration:" none", color: "#333"}}> Arvind Silswal</a></div>
+                     </li></ul>
                       </div>
                     </div>
                   <div className="row">
                      <div className="info">
-                    <i className="fa-solid fa-phone"></i>
-
-                      <div className="sub-title"><a href="tel:+917018924210" style={{textdecoration: "none", color:" #333"}}>+91-7018924210</a></div>
-                    </div>
+                      <ul style={{display:"flex",listStyle:"none"}}><li> <i className="fa-solid fa-phone"></i></li>
+                      <li> <div className="sub-title"><a href="tel:+917018924210" style={{textdecoration: "none", color:" #333"}}>+91-7018924210</a></div>
+               </li></ul>
+                  
+                          </div>
                   </div>   
             <div className="row">
                      <div className="info">
-                    <i className="fa-solid fa-envelope"></i>
+                      <ul style={{display:"flex" , listStyle:"none"}}>
+                        <li> <i className="fa-solid fa-envelope"></i></li>
+                      <li><div className="sub-title"><a href="mailto:arvindsilswal009@gmail.com" style={{textdecoration: "none", color:" #333"}}>arvindsilswal009@gmail.com</a></div>
+                 </li>
+                      </ul>
                    
-                      <div className="sub-title"><a href="mailto:arvindsilswal009@gmail.com" style={{textdecoration: "none", color:" #333"}}>arvindsilswal009@gmail.com</a></div>
-                    </div>
+                   
+                         </div>
                   </div>
       </div>
       </div>
+      <br/>
+      <hr style={{height:"5px" ,backgroundColor:"black"}}/>
+      <br/>
+      <div style={{textAlign:"center", padding:"30px"}}>
+        <h3>Thank You for Visiting My Portfolio!</h3>
+        <p>I appreciate your time and interest in exploring my work. If you have any questions, feedback, or would like to connect, please feel free to reach out to me through the contact information provided above. I'm always open to new opportunities and collaborations. Looking forward to hearing from you!</p>
                </div>  
               </div>
              
-           
+           </div>
         </section>
       
     )
--- src/component/Projects.js
+++ src/component/Projects.js
@@ -7,7 +7,8 @@ import react from "react";
             <div className="carousel owl-carousel">
               <div className="card">
                 <div className="box">
-                  <img src="/images/calculator.jpg" alt=""/>
+                 <img src={process.env.PUBLIC_URL + "/images/calculator.jpg"} alt="" />
+
                   <div className=" text">Calculator</div>
                   <p>This is a Calculator app for calculating mathematical calculations .
                     technologies used in developing the calculator are html, css, Bootstrap,JavaScript, react.js .
@@ -18,7 +19,8 @@ import react from "react";
               </div>
                 <div className="card">
                 <div className="box">
-                  <img src="/images/weatherapp.jpg" alt=""/>
+                  <img src={process.env.PUBLIC_URL + "/images/weatherapp.jpg"} alt="" />
+                  <div className="text">Weather App</div>
                   <p>This is a weather app website developed using Html , css , bootstrap ,javascript ,and with library like React .js . we can know teh weather conditions of any city just by searching the city name,it shows current weather condition with the help of weather api</p>
                    <a className="btn" href=" https://arvindsilswal009-svg.github.io/Weatherapp/">Live Link</a>&emsp;&emsp;
                             <a className="btn" href="https://github.com/arvindsilswal009-svg/Weatherapp">Code Link</a>
@@ -27,10 +29,10 @@ import react from "react";
               </div>
               <div className="card">
                 <div className="box">
-                  <img src="/images/portfolio.jpg" alt=""/>
+                 <img src={process.env.PUBLIC_URL + "/images/portfolio.jpg"} alt="" />
                   <div className="text">Portfolio</div>
                   <p>Here is my portfolio.</p>
-                   <a className="btn" href=" https://arvindsilswal009-svg.github.io/MYportfolio/">Live Link</a>&emsp;&emsp;
+      
                               <a className="btn" href=" https://github.com/arvindsilswal009-svg/newportfolio">Code Link</a>
                      
                 </div>
